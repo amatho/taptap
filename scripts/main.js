68ea45538ce4a51799c557d044bc09d151146e64
Amandus Søve Thorsrud <ama.thorsrud@gmail.com>
2017-10-23 10:37:06 +0200

    Add toolbar and tap testing

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,15 @@
 const infoCards = document.querySelector('.info-cards');
 const shadow = document.querySelector('.shadow');
+const tapArea = document.getElementById('tap-area');
+
+const LEFT_HAND = 0;
+const RIGHT_HAND = 1;
+let activeHand = LEFT_HAND;
+let testingTaps = false;
+const data = {
+  0: {},
+  1: {}
+};
 
 infoCards.querySelectorAll('.card button.btn').forEach(button => {
   button.addEventListener('click', evt => {
@@ -11,9 +21,32 @@ infoCards.querySelectorAll('.card button.btn').forEach(button => {
 
     if (nextCards.length <= 0) {
       shadow.classList.add('disabled');
+      startTapTesting();
       return;
     }
 
     nextCards[0].classList.remove('hidden');
   });
 });
+
+data[LEFT_HAND].taps = 0;
+data[RIGHT_HAND].taps = 0;
+
+tapArea.addEventListener('click', evt => {
+  if (testingTaps) {
+    data[activeHand].taps += 1;
+  }
+
+  tapArea.innerHTML = `
+  You have tapped ${data[activeHand].taps} times!
+  `;
+});
+
+function startTapTesting() {
+  fingerName = activeHand ? 'right' : 'left';
+  tapArea.innerHTML = `
+  <h1>Testing your ${fingerName} index finger</h1>
+  <h2>Start tapping to start the test!</h2>
+  `;
+  testingTaps = true;
+}
