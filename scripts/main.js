bf3309c64964e1ba26069e17e1697d103632c7ae
Amandus Søve Thorsrud <ama.thorsrud@gmail.com>
2017-10-23 13:37:20 +0200

    Start working on results

--- scripts/main.js
+++ scripts/main.js
@@ -18,13 +18,13 @@ let timeStarted;
 let _taps = 0;
 
 nextButton.addEventListener('click', evt => {
-  console.log('click');
+  // Transform away the card and start the main program
   infoCard.classList.add('done');
   startTapTesting();
 });
 
 tapArea.addEventListener('click', evt => {
-  if (!timeStarted) {
+  if (!timeStarted && testingTaps) {
     timeStarted = new Date();
     checkTimelimit();
   }
@@ -40,9 +40,8 @@ tapArea.addEventListener('click', evt => {
 });
 
 function startTapTesting() {
-  fingerName = activeHand ? 'right' : 'left';
   tapArea.innerHTML = `
-  <h1>Testing your ${fingerName} index finger</h1>
+  <h1>Testing your ${activeHand} index finger</h1>
   <h2>Start tapping to start the test!</h2>
   `;
 
@@ -53,33 +52,37 @@ function startTapTesting() {
 
 function checkTimelimit() {
   let interval = setInterval(() => {
-    if (new Date() - timeStarted > 10000) {
+    if (new Date() - timeStarted > 1000) {
       console.log('outta time');
       testingTaps = false;
       timeStarted = false;
       clearInterval(interval);
 
-      if (data[activeHand].tries >= 4) {
+      if (data[RIGHT_HAND].tries >= 4) {
+        let tryNum = data[activeHand].tries;
+        data[activeHand]['try' + tryNum] = _taps;
+
+        testingTaps = false;
+        showResults();
+      } else if (data[activeHand].tries >= 4) {
+        let tryNum = data[activeHand].tries;
+        data[activeHand]['try' + tryNum] = _taps;
+
         activeHand = RIGHT_HAND;
         prepareNextTry();
-      } else if (data[RIGHT_HAND].tries >= 4) {
-        testingTaps = false;
-        return;
       } else {
-        data[activeHand].tries += 1;
-        prepareNextTry();
-      }
+        let tryNum = data[activeHand].tries;
+        data[activeHand]['try' + tryNum] = _taps;
 
+        prepareNextTry();
+        data[activeHand].tries += 1;
+      }
     }
   }, 0.1);
 }
 
 function prepareNextTry() {
   testingTaps = false;
-  let tryNum = data[activeHand].tries;
-  let taps = data[activeHand].taps;
-  data[activeHand]['try' + tryNum] = taps;
-  data[activeHand].taps = 0;
 
   tapArea.innerHTML = `
   Get ready for your next try on your ${activeHand} index finger! Tap the screen to start.
@@ -88,4 +91,55 @@ function prepareNextTry() {
   setTimeout(() => {
     testingTaps = true;
   }, 1000);
+}
+
+function showResults() {
+  let leftTriesKeys = Object.keys(data[LEFT_HAND]).filter(key => {
+    return key.indexOf('key') !== -1;
+  });
+  let rightTriesKeys = Object.keys(data[RIGHT_HAND]).filter(key => {
+    return key.indexOf('key') !== -1;
+  });
+
+  let sum = 0;
+  let leftAverage;
+  let rightAverage;
+
+  for (let i = 0; i < 5; i++) {
+    sum += data[LEFT_HAND]['try' + i];
+  }
+
+  leftAverage = sum / 5;
+  sum = 0;
+
+  for (let i = 0; i < 5; i++) {
+    sum += data[RIGHT_HAND]['try' + i];
+  }
+
+  rightAverage = sum / 5;
+
+  let tbody = '';
+  for (let i = 0; i < 5; i++) {
+    let tr = `<tr>Try ${i + 1}`;
+    let left;
+    let right;
+    for (let j = 0; j < 2; j++) {
+      if (j = 0) {
+        left = `<td>${data[LEFT_HAND]['try' + i]}</td>`;
+      } else {
+        right = `<td>${data[RIGHT_HAND]['try' + i]}</td>`;
+      }
+    }
+    tr += '${left}${right}</tr>';
+    tbody += tr;
+  }
+
+  tapArea.innerHTML = `
+  <h1>Your results</h1>
+  <table>
+    <tbody>
+      ${tbody}
+    </tbody>
+  </table>
+  `;
 }
