23a7984563f850bfa82852fc636421f6c36ffd1b
Amandus Søve Thorsrud <ama.thorsrud@gmail.com>
2017-10-23 09:43:43 +0200

    Remove shadow after going through info cards

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,19 @@
 const infoCards = document.querySelector('.info-cards');
+const shadow = document.querySelector('.shadow');
 
 infoCards.querySelectorAll('.card button.btn').forEach(button => {
   button.addEventListener('click', evt => {
     evt.target.parentElement.classList.add('done');
     const nextCards = Array.prototype.filter.call(infoCards.children, card => {
-      return card !== evt.target.parentElement;
+      return card !== evt.target.parentElement &&
+        !card.classList.contains('done');
     });
+
+    if (nextCards.length <= 0) {
+      shadow.classList.add('disabled');
+      return;
+    }
+
     nextCards[0].classList.remove('hidden');
   });
 });
